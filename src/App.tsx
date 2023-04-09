139b30269b28a783fd90c01efa8e30995e5aeeef
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-04-10 02:22:54 +0800

    Learn the react typescript 6
    Learn the Event Props with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@
 import './App.css';
 import { Greet } from './components/Greet';
 import Heading from './components/Heading';
+import { Button } from './components/Button';
 import Oscar from './components/Oscar';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
+import { Input } from './components/Input';
 
 function App() {
 
@@ -32,6 +34,12 @@ function App() {
 
   return (
     <div className="App">
+      <Button handleClick={(event , id) => {
+        console.log('Button Clicked' , event , id)
+      }
+      }/>
+      <Input value='' handleChange={event => console.log(event)} />
+
       {/* <Status status='loading'/>
       <Heading>
          Placeholder text
