2ad0e6ce76f0161e42138741d9c84ed69291a229
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-04-10 00:07:07 +0800

    Learn the react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  }
   return (
     <div className="App">
       <Greet name={'Kwyleon'} messageCount={20} isLoggedIn={false}/>
+      <Person name={personName}/>
     </div>
   );
 }
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,3 +1,12 @@
-export const Person = () => {
-    return <div>Bruce Wayne</div>
+type PersonProps = {
+    name:{
+        first: string,
+        last: string
+    }
+}
+
+export const Person = (props: PersonProps) => {
+    return <div>
+        { props.name.first } { props.name.last }
+        </div>
 }
